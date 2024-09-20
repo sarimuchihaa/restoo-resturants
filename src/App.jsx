b807bd57b502fc0a5b0cda6525d61a21e80cf46a
sarim <sarim1@gmail.com>
2024-09-20 12:57:04 +0500

    Admin again deleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,5 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import LoginPage from './Components/Admin/LoginPage.jsx';
-import AdminDraftView from './Components/Admin/AdminDraftView.jsx';
-import OrderPage from './Components/Admin/OrderPage.jsx';
-import ResturantDetails from './Components/Admin/ResturantDetails.jsx';
-import AdminSettings from './Components/Admin/AdminSettings.jsx';
-import ReservationNotification from './Components/Admin/ReservationNotification.jsx';
-import AllReservations from './Components/Admin/AllReservations.jsx';
-import ReservationDetails from './Components/Admin/ReservationDetails.jsx';
-import AddPromotionPage from './Components/Admin/AddPromotion.jsx';
-import AdditionalAssets from './Components/Admin/AdditionalAssets.jsx';
 import SuperAdminDraftView from './Components/SuperAdmin/SuperAdminDraftView.jsx';
 import ManageVendor from './Components/SuperAdmin/ManageVendor.jsx';
 import AddNewVendor from './Components/SuperAdmin/AddNewVendor.jsx';
